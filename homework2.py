bb724923734f94e9708bb47aa4cbc3c2d150b1c1
Pasha110788 <bodzin1988@gmail.com>
2024-07-29 16:41:42 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+accent_homework = 12
+accent_hours = 1.5
+course_name = 'Python'
+time_for_assign = 0.125
+print('Курс:',course_name,',''всего задач:',accent_homework,',''затрачено часов',accent_hours,',''среднее время выполнения',time_for_assign)
